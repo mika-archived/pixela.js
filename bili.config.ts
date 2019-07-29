a75180fd0d6ef69b7f00deae7b22a7d882442a5a
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-07-29 22:14:32 +0900

    fix(bili): fix for UMD build

--- bili.config.ts
+++ bili.config.ts
@@ -3,12 +3,16 @@ import { Config } from "bili";
 const config: Config = {
   input: "src/index.ts",
   output: {
-    format: ["cjs", "es", "umd-min"]
+    format: ["cjs-min", "esm-min", "umd-min"]
   },
   plugins: {
     typescript2: {
       cacheRoot: "./.cache"
     }
+  },
+  extendRollupConfig: config => {
+    config.outputConfig.name = "Pixela";
+    return config;
   }
 };
 
